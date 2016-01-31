a66b5d2fb65aa92bd5ca667be769b3d898f34446
Sam Sobell <sam.sobell@gmail.com>
2016-01-30 16:52:12 -0800

    Added photon and enhanced attacking logic

--- Assets/character_controller.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;using System.Collections;using UnityEngine.UI;public class character_controller : MonoBehaviour {    public Animator animator;    public target_selector target_selector;    public float damage;
-    // Use this for initialization
-    void Start () {	    	}		// Update is called once per frame	void Update () {	}    public void Attack ()    {        animator.Play("Attack");        if (target_selector.target)
-        {
-            target_selector.target.BroadcastMessage("Damage", damage);
-        }        else
-        {
-            print("Nothing selected!");
-        }    }}
