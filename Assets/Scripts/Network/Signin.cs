b173162bb4d29f564bee22ca76bd9dd35f0f90aa
Sam Sobell <sam.sobell@gmail.com>
2016-01-31 22:57:44 -0800

    added android login support for real

---
+++ Assets/Scripts/Network/Signin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Signin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        MultiplayerController.Instance.TrySilentSignIn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        MultiplayerController.Instance.SignInAndStartMPGame();
+    }
+}
