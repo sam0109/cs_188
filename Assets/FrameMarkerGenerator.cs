f2c7e2c4ba9201c299841de9da903869797e3795
Sam Sobell <sam.sobell@gmail.com>
2016-01-31 19:07:54 -0800

    framemarker auto gen

---
+++ Assets/FrameMarkerGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FrameMarkerGenerator : MonoBehaviour {
+    public int marker_count;
+    public GameObject frame_marker;
+    List<GameObject> marker_list;
+	// Use this for initialization
+	void Start () {
+	    for (int i = 0; i < marker_count; i++)
+        {
+            marker_list.Add(Instantiate(frame_marker));
+            marker_list[i].GetComponent<Vuforia.MarkerBehaviour>()
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
