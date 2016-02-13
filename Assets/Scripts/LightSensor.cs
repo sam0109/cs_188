104b6d07733c313e552ca990e3802dc9348fe6ae
chanbrown007 <chanbrown007@yahoo.com>
2016-02-13 14:40:52 -0800

    Begin light

---
+++ Assets/Scripts/LightSensor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSensor : MonoBehaviour
+{
+    private string platform;
+
+	void Start ()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            platform = "android";
+        }
+        else if(Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            platform = "ios";
+        }
+    }
+	
+	void Update ()
+    {
+	
+	}
+
+    public string getPlatformType()
+    {
+        return platform;
+    }
+}
