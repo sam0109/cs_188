72a0c758dbe0ba3b45728cdac39f63575b231b34
Sam Sobell <sam.sobell@gmail.com>
2016-02-13 14:37:37 -0800

    forced portrait, removed constants, and created character save script

--- Assets/CreateCharacter.cs
+++ Assets/CreateCharacter.cs
@@ -3,13 +3,8 @@ using System.Collections;
 
 public class CreateCharacter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void SaveCharacter ()
+    {
+        
+    }
 }
