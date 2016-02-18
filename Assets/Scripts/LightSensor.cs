c3088a48de58b70e83d56f5e89400fe9edf8b9ee
chanbrown007 <chanbrown007@yahoo.com>
2016-02-18 13:47:23 -0800

    Hopefully light sensor works???

--- Assets/Scripts/LightSensor.cs
+++ Assets/Scripts/LightSensor.cs
@@ -1,22 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.iOS;
+using UnityEngine.UI;
 
 public class LightSensor : MonoBehaviour
 {
     private string platform;
+    AndroidJavaClass androidClass;
+    double sensorValue;
+    public Text found;
+    public Text light;
 
-	void Start ()
+    void Start ()
     {
         if (Application.platform == RuntimePlatform.Android)
         {
             platform = "android";
+            AndroidJNI.AttachCurrentThread();
+            androidClass = new AndroidJavaClass("cksgame.cs188.com.ambientlightsensor");
+            object[] args = new[] { this.gameObject.name, "checkSensor" };
+            androidClass.Call("initSensor", args);
+            androidClass.Call("startSensor");
         }
         else if(Application.platform == RuntimePlatform.IPhonePlayer)
         {
             platform = "ios";
         }
-        AndroidJavaClass cls = new AndroidJavaClass("android.hardware.SensorManager");
+
+    }
+
+    public void checkSensor(string message)
+    {
+        if (message == "Update Sensor")
+        {
+            sensorValue = androidClass.CallStatic<double>("getSensorValue");
+            light.text = "" + sensorValue;
+        }
+        else if(message == "Sensor Exists")
+        {
+            found.text = "FOUND!";
+        }
+        else if(message == "Sensor Not Found")
+        {
+            found.text = "DNE";
+        }
     }
 
     public string getPlatformType()
